3427e4cabe089e72f67edeec18b06c409afce4ec
Lars <lartschey@googlemail.com>
2020-06-02 10:28:42 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,38 @@
+import java.lang.System;
+import java.net.URLDecoder;
+
+/*
+* @author lartsch
+*/
+
+public class Main {
+	// get the platform string
+	public static String systemName = System.getProperty("os.name").toLowerCase();
+	public static void main(String[] args) throws Exception {
+		// check if an argument was passed on jar execution
+	    if (args.length == 0) {
+	    	// get the path of the currently running jar
+	    	final String jarPath = Main.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+	    	final String decodedPath = URLDecoder.decode(jarPath, "UTF-8");
+	    	// Setting for the terminal window title (Linux/Windows)
+	    	final String windowTitle = "App Name";
+	    	// Check the current platform...
+	    	if(systemName.contains("windows")) {
+	    		// then start the new process with the OS or terminal dependent commands
+	    		new ProcessBuilder(new String[] {"cmd", "/k", "start", windowTitle, "java", "-jar", decodedPath.substring(1), "run"}).start();
+	    	} else if(systemName.contains("mac")) {
+	    		new ProcessBuilder(new String[] {"/bin/bash", "-c", "java", "-jar", decodedPath, "run"}).start();
+	    	} else if(systemName.contains("linux")) {
+	    		// TODO: add support for other Linux terminals
+	    		new ProcessBuilder(new String[] {"xfce4-terminal", "--title="+windowTitle, "--hold", "-x", "java", "-jar", decodedPath, "run"}).start();
+	    	} else {
+	    		// No OS could be detected
+	    		System.err.println("OS could not be detected.");
+	    	}
+	    	// destroy the original process
+	    	System.exit(0);
+	    } else {
+	    	// ACTUAL PROGRAM TO EXECUTE COMES HERE
+	    } 
+	}
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+# java-selfconsole
+Double-clicking a jar file to open it is commonly associated with the "javaw" command and does not show a console upon start.
+Yet in some cases,
+* you don't want the end user to have to open the jar directly through a console,
+* it might not be possible or allowed to implement a custom console using Swing, JavaFX or other GUI-related libraries,
+* shipping bridge starting files (like bat, sh, etc.) can suck.
+
+This code snippet is exactly for those cases, where you need to make sure that double clicking the jar does actually open a console window where the program runs. It basically works by getting the path of the executed jar during run and then creating a new process by opening itself with OS dependent commands / terminals. This usually works nicely across Windows (cmd) and Mac (terminal). For Linux, as there are many distributions containing different terminal binaries, there is some terminal detection work to do.
+
+Small but nice extra: Setting the terminal title (Windows and Mac only as of now)
+
+Note: When shipping, take care of the compiler compatibility level / compiler version.
+If you want to execute your program from an IDE simply add any program run argument, otherwise it will fail to launch.
+
+**Currently on Linux, only xfce4-terminal is supported. Feel free to start a pull request with additional terminal commands / solution for detecting binaries.**
+
+Mac commands are untested but should work for most systems. Feedback would be appreciated,
+
+Use as you like lol
